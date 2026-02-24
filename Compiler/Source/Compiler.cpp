faa775670ff315bf54463c4067c1e400a7a0fcc7
CassidyHeulings <cassjheuls@gmail.com>
2026-02-24 17:46:49 -0500

    Reading input from file

--- Compiler/Source/Compiler.cpp
+++ Compiler/Source/Compiler.cpp
@@ -1,5 +1,6 @@
 // language
 #include <iostream>
+#include <fstream>
 using namespace std;
 // headers
 #include "../Headers/Logger.hpp"
@@ -8,9 +9,25 @@ int main() {
   // construct each class instance
   Logger logger(true);
 
+  // collect input
+  std::ifstream inputFile("Compiler/Input.txt");
+  std::string line;
+  std::string programs;
+  // make sure text file is found
+  if (!inputFile.is_open()) {
+      logger.info("Error: Input.txt not found.");
+      return 1;
+  }
+  // read the file
+  while (std::getline(inputFile, line)) {
+      programs += line;
+  }
+
+  inputFile.close(); // Close the file stream
+
   // start the program
-  logger.debug("This is a debug statement.");
-  logger.info("This is an info statement.");
+  logger.info("Reading programs.");
+  logger.debug("Input: " + programs);
 
   return 0;
 }
